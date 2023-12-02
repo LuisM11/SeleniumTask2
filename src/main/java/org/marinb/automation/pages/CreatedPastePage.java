99ca6c96017a332acc679d05800930b2f215aa09
LuisM11 <lumarinb@unal.edu.co>
2023-12-02 12:13:18 -0500

    new method to handle exceptions when waiting for an element

--- src/main/java/org/marinb/automation/pages/CreatedPastePage.java
+++ src/main/java/org/marinb/automation/pages/CreatedPastePage.java
@@ -2,8 +2,7 @@ package org.marinb.automation.pages;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 
 public class CreatedPastePage extends AbstractPage{
@@ -16,13 +15,27 @@ public class CreatedPastePage extends AbstractPage{
     public CreatedPastePage(WebDriver driver) {
         super(driver);
     }
+    private void waitElementHandlingException(WebElement element){
+        try{
+            waitElementToBeVisible(element);
+        }catch (TimeoutException e) {
+            logger.error("Timeout exceeded: Element is most likely not present" + e.getMessage());
+        }
+
+        try{
+            element.isDisplayed();
+        }
+        catch (NoSuchElementException | StaleElementReferenceException e){
+            logger.error("Element not Found: " + e.getMessage() );
+        }
+    }
 
     public WebElement getHighlightingTag() {
-        waitElementToBeVisible(highlightingTag);
+        waitElementHandlingException(highlightingTag);
         return highlightingTag;
     }
     public WebElement getParagraph() {
-        waitElementToBeVisible(paragraph);
+        waitElementHandlingException(paragraph);
         return paragraph;
     }
 
